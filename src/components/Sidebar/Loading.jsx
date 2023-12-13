203acf7537f37eb381b9d729d231091c9cd3dbc1
AviShi <avishihora@gmail.com>
2023-12-13 10:02:50 +0530

    deploy responsive

--- src/components/Sidebar/Loading.jsx
+++ src/components/Sidebar/Loading.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Loading = () => {
   return (
     <div class="waviy">
-      <span style="--i:1">L</span>
-      <span style="--i:2">o</span>
-      <span style="--i:3">a</span>
-      <span style="--i:4">d</span>
-      <span style="--i:5">i</span>
-      <span style="--i:6">n</span>
-      <span style="--i:7">g</span>
-      <span style="--i:8">.</span>
+      <span >L</span>
+      <span >o</span>
+      <span >a</span>
+      <span >d</span>
+      <span >i</span>
+      <span >n</span>
+      <span >g</span>
+      <span >.</span>
     </div>
   );
 }
